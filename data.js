36a557da10c1ed84329f26391460070d6c672350
Your Name <yourrealemail@gmail.com>
2026-05-03 13:09:11 +0100

    MyHouseCost first version

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+  buildingTypes: {
+    basic: 120000,
+    standard: 180000,
+    luxury: 300000
+  },
+  locations: {
+    lagos: 1.2,
+    abuja: 1.15,
+    ibadan: 1.0,
+    kano: 0.95
+  }
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PAYSTACK_SECRET_KEY = "sk_test_14c5aa3e136263f26add07ed7156e32e0c1857ab";
+
+app.post("/pay", async (req, res) => {
+  const { email, amount } = req.body;
+
+  try {
+    const response = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      {
+        email: email,
+        amount: amount * 100
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (error) {
+    console.log("PAYSTACK ERROR:", error.response?.data || error.message);
+
+    res.status(500).json({
+      status: false,
+      message: error.response?.data || error.message
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
